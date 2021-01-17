94fb81af3862fe167fab3640e6f794099c77b581
pawel <pawelzakieta97@gmail.com>
2021-01-17 18:08:25 +0100

    energy data preprocessing

---
+++ energy_preprocessing.R
@@ -0,0 +1,97 @@
+library(tidyverse)
+library(randomForest)
+library(caret)
+source ("vectorize.R") 
+
+features_importance <- function(df, label){
+  columnName <- label
+  modelFormula <- paste(columnName, " ~ .")
+  
+  rf_classifier = randomForest(as.formula(modelFormula), data = df, importance = TRUE)
+  
+  features_value = as.data.frame(importance(rf_classifier, type=1))
+  features_value = rownames_to_column(features_value)
+  colnames(features_value) <- c("col", "importance")
+  features_value <- features_value %>% arrange(desc(importance))
+  
+  return(features_value)
+}
+
+
+df_energy = read.csv("energydata_complete.csv", sep =",", header = TRUE)
+
+df_energy_filtered = select(sample_n(df_energy,nrow(df_energy)), -date)
+df_energy_scaled = data.frame(scale(df_energy_filtered))
+df_values = features_importance(df_energy_filtered,"Appliances" )
+
+# 1       lights  94.916009
+# 2  Press_mm_hg  53.632412
+# 3         RH_5  49.038540
+# 4         RH_8  44.190941
+# 5         RH_9  40.463952
+# 6         RH_1  39.401690
+# 7         RH_3  38.546811
+# 8           T5  37.238943
+# 9       RH_out  36.712892
+# 10          T8  36.548204
+# 11          T2  35.968342
+# 12  Visibility  35.933638
+# 13          T7  34.815275
+# 14   Tdewpoint  34.470642
+# 15          T3  33.954845
+# 16        RH_7  33.530282
+# 17        RH_6  32.720062
+# 18          T1  32.443242
+# 19       T_out  31.849345
+# 20        RH_4  30.747574
+# 21          T4  29.885264
+# 22          T9  28.063390
+# 23        RH_2  27.804554
+# 24   Windspeed  27.473486
+# 25          T6  27.435485
+# 26         rv2   7.501337
+# 27         rv1   7.020106
+
+# VECTORIZATION
+
+functions = list(mean=mean, min=min, max=max, sd=sd, q=quantile, sum=sum)
+
+aggregations = list(Appliances=c("sum", "mean"),
+                    lights=c("mean", "sd", "q"), 
+                    T1 = c("mean", "sd", "q"),
+                    T2 = c("mean", "sd", "q"),
+                    T3 = c("mean", "sd", "q"),
+                    T4 = c("mean", "sd", "q"),
+                    T5 = c("mean", "sd", "q"),
+                    T6 = c("mean", "sd", "q"),
+                    T7 = c("mean", "sd", "q"),
+                    T8 = c("mean",  "sd", "q"),
+                    T9 = c("mean", "sd", "q"),
+                    T_out=c("mean", "sd", "q"),
+                    RH_1 = c("mean", "sd", "q"),
+                    RH_2 = c("mean", "sd", "q"),
+                    RH_3 = c("mean", "sd", "q"),
+                    RH_4 = c("mean", "sd", "q"),
+                    RH_5 = c("mean", "sd", "q"),
+                    RH_6 = c("mean", "sd", "q"),
+                    RH_7 = c("mean", "sd", "q"),
+                    RH_8 = c("mean", "sd", "q"),
+                    RH_9 = c("mean", "sd", "q"),
+                    RH_out=c("mean", "sd", "q"),
+                    Press_mm_hg=c("mean", "sd", "q"),
+                    Windspeed=c("mean", "sd", "q"),
+                    Visibility=c("mean", "sd", "q"),
+                    Tdewpoint=c("mean", "sd", "q"),
+                    rv1=c("mean", "sd", "q"),
+                    rv2=c("mean", "sd", "q")
+                    )
+aggregated_df3 = vectorize(df_energy_filtered, 3, aggregations, functions)
+write.csv(aggregated_df3, "energy_win3.csv")
+aggregated_df6 = vectorize(df_energy_filtered, 6, aggregations, functions)
+write.csv(aggregated_df6, "energy_win6.csv")
+aggregated_df12 = vectorize(df_energy_filtered, 12, aggregations, functions)
+write.csv(aggregated_df12, "energy_win12.csv")
+aggregated_df24 = vectorize(df_energy_filtered, 24, aggregations, functions)
+write.csv(aggregated_df24, "energy_win24.csv")
+
+
--- vectorize.R
+++ vectorize.R
@@ -1,5 +1,4 @@
 library(ramify)
-library (roxygen2)
 library(purrr)
 
 letters <- LETTERS[1:24]
@@ -8,6 +7,27 @@ numbers_random <- runif(24,0,1.0)
 
 df = data.frame(letters, numbers, numbers_random);
 # df = data.frame(letters, numbers);
+
+
+# SPLITS DATAFRAME INTO A NUMBER OF SMALLER DATAFRAMES WITH CONTINUOUS DATA
+split_dataframe <-function(dataframe, time_column){
+  #datetimes = t(as.data.frame(strsplit(dataframe[[time_column_name]], ' ')))
+  #datetimes = chron(datetimes[,1], datetimes[,2], format=c('y-m-d','h:m:s'))
+  #time_differences = as.numeric(datetimes[2:length(datetimes)] - datetimes[1:length(datetimes)-1])
+  time_differences = as.numeric(time_column[2:length(datetimes)] - time_column[1:length(time_column)-1])
+  dt = as.numeric(names(sort(table(time_differences),decreasing=TRUE)[1]))
+  margin = 0.2
+  chunk_borders = append(which(time_differences>dt*(1+margin)), length(time_differences)+1)
+  result = c()
+  chunk_begining = 1
+  idx = 1
+  for (i in chunk_borders){
+    result[[idx]] = dataframe[chunk_begining:i-1,]
+    idx = idx + 1
+    chunk_begining = i
+  }
+  return(result)
+}
 
 vectorize<-function(dataframe, window_size, aggregations, functions){
   attr_names = names(aggregations)
@@ -22,7 +42,8 @@ vectorize<-function(dataframe, window_size, aggregations, functions){
       attr_aggregation = functions[[attr_aggregation_name]]
       aggregated_values = data.frame(apply(windowed, 1, attr_aggregation))
       if (ncol(aggregated_values)>1){
-        aggregated_values = t(aggregated_values)}
+        aggregated_values = t(aggregated_values)
+      }
       if (length(result)>0){
         result = cbind(result, aggregated_values)
       }
@@ -30,7 +51,8 @@ vectorize<-function(dataframe, window_size, aggregations, functions){
         result = data.frame(aggregated_values)
       }
       for (i in 1:ncol(aggregated_values)){
-        colnames(result)[col_idx] <- paste(attr_name, attr_aggregation_name)
+        if (ncol(aggregated_values)>1) colnames(result)[col_idx] <- paste(attr_name, attr_aggregation_name, i)
+        else colnames(result)[col_idx] <- paste(attr_name, attr_aggregation_name)
         col_idx = col_idx + 1
       }
     }
@@ -51,6 +73,6 @@ functions <- list(sum=sum, mean=mean, nic=multi_fun, count=partial(count_occuran
 
 # Assigning function abbreviations to each attribute
 #aggregations<-list(letters=list("frequent", "frequent"), numbers = list("sum", "mean", "nic"))
-aggregations<-list(letters = list("count"), numbers=list("mean", "sum"), numbers_random=list("mean", "sum"))
+aggregations<-list(letters = c("count"), numbers=c("mean", "sum"), numbers_random=c("mean", "sum"))
 result = vectorize(df, 3, aggregations, functions)
 a = 1
